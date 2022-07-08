2c88509a5b1fc868c00ff7a83b85504f54f98ff0
KANKİNOU SELONOU GAUTİER <kankinounoatedegautier@yahoo.fr>
2022-07-08 08:04:45 +0300

    Update awk_extra_loop.sh

--- awk_extra_loop.sh
+++ awk_extra_loop.sh
@@ -1,3 +1,4 @@
+#This script allows to grep the number of atoms in sdf files and put of print all number with molecule name in same sdf file
 while read -r mol;
 	do awk 'NR==4' "$mol".sdf > out1.csv && awk '{print $1}' out1.csv > out2.csv && rm out1.csv && echo "$mol" >> out2.csv && csvtool transpose out2.csv > "$mol".csv && rm out2.csv;
 done < listes_all.txt 	
